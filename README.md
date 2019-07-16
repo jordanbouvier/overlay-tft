89c4e3c6e2aebd9d6ee9ecae4696bc4dde1a16e2
jordanbouvier <jordanbouvierpro@gmail.com>
2019-07-16 07:27:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Overlay TFT
+Here's a simple League of legends TFT overlay 
+
+[Video Preview](https://www.youtube.com/watch?v=VmWtxOsEjJ0&list=UU_fG6B6Z4KbBWvw2UP1rXAw)
+
+Step 1: 
+
+![step 1](install-images/1.png)
+
+Step 2: 
+
+![step 2](install-images/2.png)
+
+Step 3: 
+
+![step 3](install-images/3.png)
+
+Step 4: 
+
+![step 4](install-images/4.png)
+
+Step 5: 
+
+![step 5](install-images/5.png)
---
+++ build/precache-manifest.5a12edf57b0ab64c39c1513a3a13fe17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5239f4484d97eb3134b708eb8f673b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12acf9349fd73ad7da42",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "12acf9349fd73ad7da42",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.svg"
+  }
+]);
